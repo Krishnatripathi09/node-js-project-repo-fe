d23f8fb3b54d48e196dfe7868af65eff281cd50b
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-01-14 10:04:59 +0530

    feat: implemented route protection for feed and profile pages based on authentication
    Description:
    - Added blocks to restrict access to the feed and profile pages for users who are not logged in.
    - Implemented token validation to ensure that users with expired tokens are redirected to the login page.
    - Enhanced security by protecting sensitive routes and ensuring proper user authentication before granting access.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
 const user = useSelector((store)=>store.user)
@@ -8,7 +9,7 @@ console.log(user)
     <>
     <div className="navbar bg-base-300">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevTinder🚀</a>
+    <Link to="/" className="btn btn-ghost text-xl">DevTinder🚀</Link>
   </div>
   <div className="flex-none">
     <div className="dropdown dropdown-end">
@@ -20,18 +21,18 @@ console.log(user)
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            alt="user photo"
+            src={user.photoUrl}/>
         </div>
       </div>
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile"className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
