33482aa8ef79e1eb4ba88f1185c217bfc92984d4
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-07-18 05:47:16 +0530

    refactor: Changes the User Card import in Edit Profile

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import UserCard from "./userCard";
+import UserCard from "./UserCard";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
